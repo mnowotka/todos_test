441bd09c5b8b3295988a3e1509b3104bef9bdae5
Michał Nowotka <michal.nowotka@bolt.eu>
2020-04-18 15:25:10 +0200

    added marshmallow

--- service.py
+++ service.py
@@ -1,7 +1,5 @@
 from urllib.parse import urljoin
-
 import requests
-
 from constants import BASE_URL
 
 
@@ -12,6 +10,7 @@ class Service:
     def get_todos(cls):
         response = requests.get(cls.TODOS_URL)
         if response.ok:
-            return response
+            return response.json()
         else:
             return None
+
--- test_todos.py
+++ test_todos.py
@@ -1,11 +1,37 @@
-from unittest.mock import patch
-
-from nose.tools import assert_is_not_none
-
+import os
+import unittest
+from unittest.mock import Mock, patch
+from nose.tools import assert_is_not_none, assert_list_equal
 from service import Service
+from schema import TodoSchema
 
-def test_getting_todos():
-    with patch('service.requests.get') as mock_get:
-        mock_get.return_value.ok = True
+
+class TestTodos(unittest.TestCase):
+
+    def setUp(self):
+        if os.getenv('TEST_REAL_API', False):
+            return
+        self.mock_get_patcher = patch('service.requests.get')
+        self.mock_get = self.mock_get_patcher.start()
+
+        self.expected_todos = [{
+            'userId': 1,
+            'id': 1,
+            'title': 'Make the bed',
+            'completed': False
+        }]
+
+        self.mock_get.return_value = Mock(ok=True)
+        self.mock_get.return_value.json.return_value = self.expected_todos
+
+    def tearDown(self):
+        if os.getenv('TEST_REAL_API', False):
+            return
+        self.mock_get_patcher.stop()
+
+    def test_getting_todos(self):
+        print("actual test!")
         todo = Service.get_todos()
         assert_is_not_none(todo)
+        assert_list_equal(todo, self.expected_todos)
+        TodoSchema().load(many=True, data=todo)
